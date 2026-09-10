028fe6f1ee448d75042bb63bd6ec9cae5745d9f7
sufal54 <sufalbala5@gmail.com>
2026-09-10 13:39:09 +0000

    added new verstion update apis

---
+++ src/app/api/update/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+type UpdateType = {
+  version: string;
+  new?: string[];
+  improved?: string[];
+  fixed?: string[];
+};
+
+export async function GET() {
+  const update: UpdateType = {
+    version: "0.1.2",
+    new: ["Solidjs as Frontend", "Optimize perfomance"],
+    improved: ["Improved UI added new motion"],
+    fixed: ["Fixed store bug"],
+  };
+
+  return NextResponse.json(update);
+}
