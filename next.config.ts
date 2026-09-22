3bce1f29605978177b1153ab8363f1d62757d9d3
sufal54 <sufalbala5@gmail.com>
2026-09-22 12:57:45 +0000

    Fix GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  // assetPrefix: "/khushi.github.io",
+  basePath: "/khushi_web",
   images: {
     unoptimized: true,
   },
